07059e5760cc2f8c2f3eeafc72c49fa7f6e53044
selligZeittom <gilmottiez@gmail.com>
2018-08-31 10:00:44 +0200

    pause

--- software/PongGame.X/Game_controller.h
+++ software/PongGame.X/Game_controller.h
@@ -22,17 +22,28 @@ typedef enum SM_ON
     ST_ON_GAME,
 } SM_ON;
 
-typedef enum SM_GAME
+typedef enum SM_SCREEN
 {
-    ST_ON_GAME_WAIT,
-    ST_ON_GAME_UPDATE_SCREEN,
-    ST_ON_GAME_TIMEOUT_BALL,
-    ST_ON_GAME_TIMEOUT_MOVE_X,
-    ST_ON_GAME_TIMEOUT_AI,
-    ST_ON_GAME_CHECK_RACKET,
-    ST_ON_GAME_MOVE_BALL,
-    ST_ON_GAME_OVER,
-} SM_GAME;
+    ST_SCREEN_WAIT,
+    ST_SCREEN_UPDATE,
+} SM_SCREEN;
+
+typedef enum SM_RACKET
+{
+    ST_RACKET_WAIT,
+    ST_RACKET_USER,
+    ST_RACKET_AI,
+} SM_RACKET;
+
+typedef enum SM_BALL
+{
+    ST_BALL_WAIT,
+    ST_BALL_CHECK_Y,
+    ST_BALL_CHECK_X,
+    ST_BALL_MOVE,
+    ST_BALL_GAME_OVER,
+} SM_BALL;
+
 
 /* 
  * this is the struct to use to control the game
@@ -45,11 +56,18 @@ typedef struct Game_controller
 {
     SM_MODE sm_mode, old_sm_mode;
     SM_ON sm_on, old_sm_on;
-    SM_GAME sm_game, old_sm_game;
+    SM_SCREEN sm_screen, old_sm_screen;
+    SM_RACKET sm_racket, old_sm_racket;
+    SM_BALL sm_ball, old_sm_ball;
 }Game_controller;
 
-
+/*initialization
+ * 
+ * put the states machines in their starting state
+ */
 void init_Game_controller(Game_controller* gameCtrl);
+
+
 void state_machine_mode(Game_controller* gameCtrl, Event ev);
 void state_machine_on(Game_controller* gameCtrl, Event ev);
 void state_machine_game(Game_controller* gameCtrl, Event ev);
